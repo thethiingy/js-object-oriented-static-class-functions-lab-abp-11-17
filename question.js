ede1fe61549e84e81ab686b00999d47576fdbf94
thethiingy <thethiingy@yahoo.com>
2017-11-12 18:00:47 +0000

    Done.

--- question.js
+++ question.js
@@ -1,1 +1,23 @@
 'use strict';
+
+class Question {
+  constructor(content) {
+    this.content = content;
+    this.save();
+    console.log(this);
+  }
+
+  static All(){
+    return this._All;
+  }
+
+  save() {
+    this.constructor._All.push(this);
+  }
+
+  static Find(id) {
+    return this._All[id-1];
+  }
+
+}
+Question._All = [];
